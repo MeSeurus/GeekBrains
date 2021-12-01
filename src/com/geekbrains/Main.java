0b2269fc96440707ea697ca2cf781a2059612a75
MeSeurus <redoreole@gmail.com>
2021-12-01 15:09:51 +0300

    Algorithms. Lesson 6

--- src/com/geekbrains/Main.java
+++ src/com/geekbrains/Main.java
@@ -1,43 +1,26 @@
 package com.geekbrains;
 
-/**
- * 1. Написать программу по возведению числа в степень с помощью рекурсии.
- * 2. Написать программу «Задача о рюкзаке» с помощью рекурсии.
- */
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Задание 1.
-        System.out.println(recursionPower(9,-1));
+        Random random = new Random();
+        int quantity = 29478;
+        int flag = 0;
 
-        //Задание 2.
-        Item[] items = {new Item(2, 3),
-                new Item(3, 5),
-                new Item(5, 3),
-                new Item(1, 7),
-                new Item(2, 9)};
-
-        Satchel satchel = new Satchel(items);
-        //Вместимость рюкзака
-        int capacity = 10;
-        System.out.println("Рюкзак заполнен на " +
-                satchel.optSum(items.length - 1, capacity) + " из " + capacity);
-    }
-
-    //1. Написать программу по возведению числа в степень с помощью рекурсии.
-
-    public static double recursionPower(double value, double power) {
-        if (value == 0) {
-            return 0;
+        for (int i = 0; i < quantity; i++) {
+            GenTreeMap<Integer, Integer> map = new GenTreeMap<>();
+            int x = 0;
+            while (map.height() < 6) {
+                x = random.nextInt(101);
+                map.put(x, x);
+            }
+            map.delete(x);
+            if (map.isBalanced()) {
+                flag++;
+            }
         }
-        if (power == 0) {
-            return 1;
-        }
-        if (power < 0) {
-            return recursionPower(1 / value, -1 * power);
-        } else {
-            return value * recursionPower(value, power - 1);
-        }
+        System.out.println("Сбалансировано " + (double) flag / quantity * 100 + " % (" + flag + " штук) из 100 % (всего " + quantity + " штук)");
     }
 }
