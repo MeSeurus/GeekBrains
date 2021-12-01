1cb3a2c4830c1f839909e627868740e8f7b517af
MeSeurus <redoreole@gmail.com>
2021-12-01 11:03:11 +0300

    Algorithms. Lesson 3

--- src/main/java/ru/gb/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import ru.gb.entity.Product;
-
-public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
-}
