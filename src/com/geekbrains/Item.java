eb33c2e58e84af1fd0af26e3c8ba4ee2897f9c00
MeSeurus <redoreole@gmail.com>
2021-12-01 15:04:22 +0300

    Algorithms. Lesson 5

---
+++ src/com/geekbrains/Item.java
@@ -0,0 +1,12 @@
+package com.geekbrains;
+
+public class Item {
+
+    int weight;
+    int price;
+
+    public Item(int price, int weight) {
+        this.weight = weight;
+        this.price = price;
+    }
+}
--- src/com/geekbrains/Main.java
+++ src/com/geekbrains/Main.java
@@ -1,12 +1,43 @@
 package com.geekbrains;
 
 /**
- * 1. Реализовать MyLinkedStack используя класс MyLinkedList
+ * 1. Написать программу по возведению числа в степень с помощью рекурсии.
+ * 2. Написать программу «Задача о рюкзаке» с помощью рекурсии.
  */
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+
+        //Задание 1.
+        System.out.println(recursionPower(9,-1));
+
+        //Задание 2.
+        Item[] items = {new Item(2, 3),
+                new Item(3, 5),
+                new Item(5, 3),
+                new Item(1, 7),
+                new Item(2, 9)};
+
+        Satchel satchel = new Satchel(items);
+        //Вместимость рюкзака
+        int capacity = 10;
+        System.out.println("Рюкзак заполнен на " +
+                satchel.optSum(items.length - 1, capacity) + " из " + capacity);
+    }
+
+    //1. Написать программу по возведению числа в степень с помощью рекурсии.
+
+    public static double recursionPower(double value, double power) {
+        if (value == 0) {
+            return 0;
+        }
+        if (power == 0) {
+            return 1;
+        }
+        if (power < 0) {
+            return recursionPower(1 / value, -1 * power);
+        } else {
+            return value * recursionPower(value, power - 1);
+        }
     }
 }
